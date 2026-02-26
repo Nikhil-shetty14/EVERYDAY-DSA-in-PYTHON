54c1a2bfb8584ad5437a4bd2e9b62f8dbc842fbd
Nihkil S Shetty <nikhilshettyvss@gmail.com>
2026-02-26 23:59:19 +0530

    Add files via upload

---
+++ DAY24/simple.py
@@ -0,0 +1,17 @@
+# 
+
+"""logic """
+
+# def is_palin(s):
+#   left = 0
+#   right = len(s) - 1
+
+#   while left < right:
+#     if s[left] != s[right]:
+#       return False
+#     left += 1
+#     right -= 1
+
+#   return True
+
+# print(is_palin("racecar"))
